5bcf02be10393432b91a2fa1f52980f61be930d3
sarracent <damiansarracent89@gmail.com>
2018-08-09 03:39:52 -0300

    Edit Main class

--- src/coreJava/Main.java
+++ src/coreJava/Main.java
@@ -6,16 +6,15 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		/*System.out.println(Palindrome.isPalindrome("ababa"));
+		System.out.println(Palindrome.isPalindrome("ababa"));
 		System.out.println(Palindrome.isPalindromeString("ababa"));
 		System.out.println(RemoveChar.removeChar("caca", 'c'));
 		System.out.println(StringPalindromeTest.checkPalindrome("abcba"));
 		StringToChar.stringToChar();
 		JavaStringSplit.stringSplit();
-		RotateArray.rotatearray();*/
-		
 		StringToByteArray.stringToByteArray();
 		ByteArrayToString.byteArrayToString();
+		RotateArray.rotatearray();
 		
 	}
 
